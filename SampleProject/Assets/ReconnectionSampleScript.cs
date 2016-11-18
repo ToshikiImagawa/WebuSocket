1b3dd2b1e2730266648c041f7467ad9727504b8c
sassembla <toru.inoue@kissaki.tv>
2016-11-19 06:17:07 +0900

    fix indent...

--- SampleProject/Assets/ReconnectionSampleScript.cs
+++ SampleProject/Assets/ReconnectionSampleScript.cs
@@ -7,56 +7,56 @@ using WebuSocketCore;
 
 /**
 	sample of websocket-disconnect then reconnect.
-    repeat connect -> disconnect -> reconnect -> disconnect... 
+	repeat connect -> disconnect -> reconnect -> disconnect... 
 */
 public class ReconnectionSampleScript : MonoBehaviour {
 
-    WebuSocket webSocket;
+	WebuSocket webSocket;
 
-    bool opened = false;
+	bool opened = false;
 
-    void Start () {
-        webSocket = new WebuSocket(
-            "wss://echo.websocket.org:443/",
-            1024,
-            () => {
-                Debug.Log("connection succeeded.");
-                opened = true;
-            },
-            datas => {},
-            () => { },
-            closeReason => {
-                Debug.Log("closed, closeReason:" + closeReason);
-                switch (closeReason) {
-                    case WebuSocketCloseEnum.CLOSED_BY_TIMEOUT: {
+	void Start () {
+		webSocket = new WebuSocket(
+			"wss://echo.websocket.org:443/",
+			1024,
+			() => {
+				Debug.Log("connection succeeded.");
+				opened = true;
+			},
+			datas => {},
+			() => { },
+			closeReason => {
+				Debug.Log("closed, closeReason:" + closeReason);
+				switch (closeReason) {
+					case WebuSocketCloseEnum.CLOSED_BY_TIMEOUT: {
 						Debug.Log("start reconnect.");
 						StartCoroutine(Reconnection(webSocket));
 						break;
 					}
-                }
-            }
-        );
-    }
+				}
+			}
+		);
+	}
 
-    private IEnumerator Reconnection (WebuSocket ws) {
-        yield return new WaitForSeconds(1);
-        webSocket = WebuSocket.Reconnect(ws);
-    }
+	private IEnumerator Reconnection (WebuSocket ws) {
+		yield return new WaitForSeconds(1);
+		webSocket = WebuSocket.Reconnect(ws);
+	}
 
-    int frame = 0;
-    void Update () {
-        // disconnect after 2sec.
-        if (opened) {
-            if (frame == 120) {
-                opened = false;
-                frame = 0;
-                webSocket.Disconnect(true, WebuSocketCloseEnum.CLOSED_BY_TIMEOUT);
-            }
-            frame++;
-        }
-    }
+	int frame = 0;
+	void Update () {
+		// disconnect after 2sec.
+		if (opened) {
+			if (frame == 120) {
+				opened = false;
+				frame = 0;
+				webSocket.Disconnect(true, WebuSocketCloseEnum.CLOSED_BY_TIMEOUT);
+			}
+			frame++;
+		}
+	}
 
-    void OnApplicationQuit() {
-        webSocket.Disconnect(true);
-    }
+	void OnApplicationQuit() {
+		webSocket.Disconnect(true);
+	}
 }
