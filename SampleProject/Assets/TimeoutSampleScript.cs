44baad49262d437ad8570110be136095970ba45f
sassembla <toru.inoue@kissaki.tv>
2016-11-21 19:54:40 +0900

    fix indent...

--- SampleProject/Assets/TimeoutSampleScript.cs
+++ SampleProject/Assets/TimeoutSampleScript.cs
@@ -11,7 +11,7 @@ using WebuSocketCore;
 public class TimeoutSampleScript : MonoBehaviour {
 
 	WebuSocket webSocket;
-    bool opened = false;
+	bool opened = false;
 
 	void Start () {
 		webSocket = new WebuSocket(
@@ -19,7 +19,7 @@ public class TimeoutSampleScript : MonoBehaviour {
 			1024,
 			() => {
 				Debug.Log("connection succeeded.");
-                opened = true;
+				opened = true;
 			},
 			datas => {},
 			() => { },
@@ -27,26 +27,26 @@ public class TimeoutSampleScript : MonoBehaviour {
 				Debug.Log("closed, closeReason:" + closeReason);
 			},
 
-            /*
-                timeout parameter.
-                -1 is not good. only for sample.
+			/*
+				timeout parameter.
+				-1 is not good. only for sample.
 
-                I think you may set large number than 0.
-            */
-            timeoutSec:-1
+				I think you may set large number than 0.
+			*/
+			timeoutSec:-1
 		);
 	}
 
-    void Update () {
-        /*
-            IsConnected() method can detect timeout of connection.
-            You can call this method by calling it at appropriate intervals.
-        */
-        if (opened && !webSocket.IsConnected()) {
-            Debug.LogError("timeout detected.");
-            opened = false;
-        } 
-    }
+	void Update () {
+		/*
+			IsConnected() method can detect timeout of connection.
+			You can call this method by calling it at appropriate intervals.
+		*/
+		if (opened && !webSocket.IsConnected()) {
+			Debug.LogError("timeout detected.");
+			opened = false;
+		} 
+	}
 
 	void OnApplicationQuit() {
 		webSocket.Disconnect(true);
