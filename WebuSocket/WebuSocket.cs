a3b13fd7cecfd462507d0940b3d60fb591b1b88c
sassembla <toru.inoue@kissaki.tv>
2017-05-31 15:56:35 +0900

    set disconnect when no valid ip found.

--- WebuSocket/WebuSocket.cs
+++ WebuSocket/WebuSocket.cs
@@ -245,15 +245,16 @@ namespace WebuSocketCore {
 
 							// choose valid ip.
 							foreach (IPAddress ipaddress in addresses.AddressList){
-								 if (ipaddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
+								 if (ipaddress.AddressFamily == AddressFamily.InterNetwork) {
 										this.endPoint = new IPEndPoint(ipaddress, port);
 										StartConnectAsync();
 										return;
 								 }
-							 }
+							}
+
 							var unresolvedAddressFamilyException = new Exception("failed to get valid address family from list.");
 							OnError(WebuSocketErrorEnum.DOMAIN_UNRESOLVED, unresolvedAddressFamilyException);
-							StartConnectAsync();
+							Disconnect();
 						}
 					),
 					this
@@ -261,7 +262,7 @@ namespace WebuSocketCore {
 				return;
 			}
 				
-			// ip.
+			// raw ip.
 			this.endPoint = new IPEndPoint(IPAddress.Parse(host), port);
 			StartConnectAsync();
 		}
