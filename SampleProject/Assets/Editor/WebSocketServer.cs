31ee98a3abb40a1f073ac92549525378b5c7ea76
sassembla <toru.inoue@kissaki.tv>
2017-06-17 12:14:22 +0900

    adding UnityTests server in Unity2017.

--- SampleProject/Assets/Editor/WebSocketServer.cs
+++ SampleProject/Assets/Editor/WebSocketServer.cs
@@ -11,11 +11,19 @@ using Alchemy;
 [InitializeOnLoad] public class WebSocketServer {
 	static WebSocketServer () {
 		Debug.LogError("start receiving");
+		
 		var s = new Alchemy.WebSocketServer(8080, IPAddress.Parse("127.0.0.1"));
 		s.OnConnect += (c) => {
 			Debug.LogError("fmm? c:" + c);
 			// OnEventDelegate
 		};
+
 		s.Start();
+
+		/*
+			これで、WebSocketを受けたりするテストが書けるようになる。
+			UnityTestで処理することができるかな〜どうだろ。
+			
+		 */
 	}
 }
